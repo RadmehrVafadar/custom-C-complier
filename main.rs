a3da88df6cc590671edeb244cdebf22e308ef36a
Radmehr <23xf1@queensu.ca>
2025-08-08 16:41:26 -0400

    Implement argument error checking

--- main.rs
+++ main.rs
@@ -1,8 +1,18 @@
 
-
+use std::fs;
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{}", args[0]);
+
+    if args.len() < 2 {
+        eprintln!("Usage: ./main <fileName>");
+        return;
+    }
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path).expect("Could not read the file");
+
+
+
+    println!("{contents}");
 }
